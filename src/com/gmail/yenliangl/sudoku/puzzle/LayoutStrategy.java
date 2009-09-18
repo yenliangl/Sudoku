9107c96164ef0adf0c58f24f46a78ad440d86fac
Liu Yen-Liang (Josh) <yenliangl@gmail.com>
2009-09-18 17:40:43 +0800

    Optimize sudoku generator.
    - Call overridden methods in constructors. Fixed.
    - Add LayoutStrategy to com.gmail.yenliangl.sudoku.puzzle.

---
+++ src/com/gmail/yenliangl/sudoku/puzzle/LayoutStrategy.java
@@ -0,0 +1,36 @@
+package com.gmail.yenliangl.sudoku.puzzle;
+
+abstract class LayoutStrategy {
+    protected LayoutStrategy() {}
+
+    public void layout(Puzzle puzzle) {
+        layoutRows(puzzle);
+        layoutColumns(puzzle);
+        layoutPentominoes(puzzle);
+    }
+
+    protected abstract void layoutPentominoes(Puzzle puzzle);
+    protected abstract int getPentominoIndexOf(final int cellCowIndex,
+                                               final int cellColumnIndex);
+
+    protected void layoutRows(Puzzle puzzle) {
+        final int dimension = puzzle.getDimension();
+        for(int rowIndex = 0; rowIndex < dimension; rowIndex++) {
+            Row row = puzzle.getRow(rowIndex);
+            for(int columnIndex = 0; columnIndex < dimension; columnIndex++) {
+                row.addCell(puzzle.getCell(rowIndex, columnIndex));
+            }
+        }
+    }
+
+    protected void layoutColumns(Puzzle puzzle) {
+        final int dimension = puzzle.getDimension();
+        for(int columnIndex = 0; columnIndex < dimension; columnIndex++) {
+            Column column = puzzle.getColumn(columnIndex);
+            for(int rowIndex = 0; rowIndex < dimension; rowIndex++) {
+                column.addCell(puzzle.getCell(rowIndex, columnIndex));
+            }
+        }
+    }
+}
+
